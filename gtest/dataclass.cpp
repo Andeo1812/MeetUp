7a4446a58d6a1a78ad7e1dccfa9a6334438a1ea6
Andeo1812 <alexanforall@gmail.com>
2022-04-16 10:22:06 +0300

    ++ test dataclass

--- gtest/dataclass.cpp
+++ gtest/dataclass.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <string>
+
 #include "User.hpp"
 #include "Address.hpp"
 #include "PersonalData.hpp"
@@ -11,31 +13,45 @@
 
 
 TEST(DATACLASS, User) {
+    std::string user_id = {"123"};
+    std::string password = {"GogozikPARSSS"};
+    std::string nickname = {"Gogozik"};
 
+    User user;
+
+    EXPECT_TRUE(user.IsEmpty());
+
+    user.SetUserId(user_id);
+    user.SetPassword(password);
+    user.SetNickname(nickname);
+
+    EXPECT_TRUE(user_id == user.GetUserId());
+    EXPECT_TRUE(user_id == user.GetPassword());
+    EXPECT_TRUE(user_id == user.GetNickname());
 }
 
 TEST(DATACLASS, Address) {
-
+  //  По аналогии с User
 }
 
 TEST(DATACLASS, PersonalData) {
-
+    //  По аналогии с User
 }
 
 TEST(DATACLASS, GeneralData) {
-
+    //  По аналогии с User
 }
 
 TEST(DATACLASS, Events) {
-
+    //  По аналогии с User
 }
 
 TEST(DATACLASS, Contacts) {
-
+    //  По аналогии с User
 }
 
 TEST(DATACLASS, Groups) {
-
+    //  По аналогии с User
 }
 
 TEST(DATACLASS, MeetUps) {
