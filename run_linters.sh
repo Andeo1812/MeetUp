577019bac68ead772586d1447d7bf6217ae12d42
Andeo1812 <alexanforall@gmail.com>
2022-04-27 20:05:38 +0300

    make check in process

--- run_linters.sh
+++ run_linters.sh
@@ -21,13 +21,27 @@ function check_log() {
     fi
 }
 
+main="project/main.cpp"
+
+Context_hpp="project/context/include/*"
+Context_cpp="project/context/src/*"
+Context_path_hpp="project/context/include/"
+
+Database_hpp="project/database/PostgreSQL/include/* project/database/include/virtual/*  project/database/include/impl/*"
+Database_cpp="project/database/PostgreSQL/src/*"
+Database_path_hpp="project/database/"
+
+Sum_hpp="${Context_hpp} ${Database_hpp}"
+Sum_cpp="${Context_cpp} ${Database_cpp} ${main}"
+Sum_path_hpp="${Context_path_hpp} ${Database_path_hpp}"
+
 print_header "RUN cppcheck"
 check_log "cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project --suppress=missingIncludeSystem" "\(information\)"
 
 print_header "RUN clang-tidy"
-check_log "clang-tidy project/general/src/* project/general/include/* -warnings-as-errors=* -extra-arg=-std=c99 -- -I project/general/include" "Error (?:reading|while processing)"
+check_log "clang-tidy ${Sum_cpp} ${Sum_hpp} -warnings-as-errors=*  -- -x c++ --std=c++20 -I ${Sum_path_hpp}" "Error (?:reading|while processing)"
 
 print_header "RUN cpplint"
-check_log "cpplint --extensions=c project/general/include/* project/pattern/imperative_model/include/* project/general/src/* project/pattern/imperative_model/src/*" "Can't open for reading"
+check_log "cpplint --extensions=cpp project/general/include/* project/pattern/imperative_model/include/* project/general/src/* project/pattern/imperative_model/src/*" "Can't open for reading"
 
 print_header "SUCCESS"
