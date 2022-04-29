f64080a1be6421f9429f23184b5eb64f8f549ed5
Andeo1812 <alexanforall@gmail.com>
2022-04-29 20:15:48 +0300

    add User Event Contacts parser

--- project/parsing/JSON/src/ParserContacts.cpp
+++ project/parsing/JSON/src/ParserContacts.cpp
@@ -33,7 +33,34 @@ Context ParserUserContacts::StrToObject(const std::string &parser_str) const {
 }
 
 std::string ParserUserContacts::ObjectToStr(const std::string type_response, const Context &other) const {
+    nlohmann::json j;
+
     std::string res;
+
+    if (type_response == ADD_USER_CONTACTS || type_response == RM_USER_CONTACTS) {
+        if (other.GetError().empty()) {
+            j[type_response] = "OK";
+            res = j.dump();
+        } else  {
+            j[type_response] = other.GetError();
+            res = j.dump();
+        }
+        return res;
+    }
+
+    Contacts contacts = other.GetContacts();
+
+    if (contacts.GetContacts().empty()) {
+        j[type_response] = "Not found contacts";
+
+        res = j.dump();
+
+        return res;
+    }
+
+    j[type_response] = contacts.GetContacts();
+
+    res = j.dump();
 
     return res;
 }
