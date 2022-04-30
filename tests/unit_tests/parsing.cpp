f500d3ed72a4582c369621e325cee62d143d6d1e
Andeo1812 <alexanforall@gmail.com>
2022-04-30 14:35:27 +0300

    parsing test in process

--- tests/unit_tests/parsing.cpp
+++ tests/unit_tests/parsing.cpp
@@ -4,6 +4,7 @@
 #include "ParserAddressData.hpp"
 #include "ParserPersonalData.hpp"
 #include "ParserGeneralData.hpp"
+#include "ParserEvent.hpp"
 #include "ParserContacts.hpp"
 #include "ParserGroup.hpp"
 #include "ParserMeetUp.hpp"
@@ -12,25 +13,27 @@
 TEST(PARSING, User) {
     std::string input = {"{\"registration\":{\"nickname\":\"Sasha\",\"password\":\"123456asd64569898989\"}}"};
     std::string type_request = {"registration"};
-    std::string key_nickname = {"Sasha"};
-    std::string key_password = {"123456asd64569898989"};
 
     ParserUser parser;
 
     Context context;
 
-    User user;
-
-    context = user;
-
     context = parser.StrToObject(input);
 
-    EXPECT_EQ(key_nickname, context.GetUser().GetNickname());
-    EXPECT_EQ(key_password, context.GetUser().GetPassword());
+    EXPECT_EQ("Sasha", context.GetUser().GetNickname());
+    EXPECT_EQ("123456asd64569898989", context.GetUser().GetPassword());
 
     std::string response = parser.ObjectToStr(type_request, context);
 
     EXPECT_EQ(input, response);
+
+    context.SetError("Test error");
+
+    std::string expected_response_error = {"{\"registration\":\"Test error\"}"};
+
+    std::string response_error = parser.ObjectToStr(type_request, context);
+
+    EXPECT_EQ(response_error, expected_response_error);
 }
 
 TEST(PARSING, AddressData) {
@@ -43,33 +46,21 @@ TEST(PARSING, AddressData) {
                          "\"index\":\"143430\", "
                          "\"country\":\"Russia\"}}"};
     std::string type_request = {"write_address"};
-    std::string user_id = {"1"};
-    std::string building = {"2"};
-    std::string housing = {"3"};
-    std::string street = {"Bolshaya Voskresenskaya"};
-    std::string city = {"Krasnogorsk"};
-    std::string district = {"Krasnogorskiy"};
-    std::string index = {"143430"};
-    std::string country = {"Russia"};
 
     ParserAddressData parser;
 
     Context context;
 
-    AddressData address_data;
-
-    context = address_data;
-
     context = parser.StrToObject(input);
 
-    EXPECT_EQ(user_id, context.GetAddressData().GetUserId());
-    EXPECT_EQ(building, context.GetAddressData().GetBuilding());
-    EXPECT_EQ(housing, context.GetAddressData().GetHousing());
-    EXPECT_EQ(street, context.GetAddressData().GetStreet());
-    EXPECT_EQ(city, context.GetAddressData().GetCity());
-    EXPECT_EQ(district, context.GetAddressData().GetDistrict());
-    EXPECT_EQ(index, context.GetAddressData().GetIndex());
-    EXPECT_EQ(country, context.GetAddressData().GetCountry());
+    EXPECT_EQ("1", context.GetAddressData().GetUserId());
+    EXPECT_EQ("2", context.GetAddressData().GetBuilding());
+    EXPECT_EQ("3", context.GetAddressData().GetHousing());
+    EXPECT_EQ("Bolshaya Voskresenskaya", context.GetAddressData().GetStreet());
+    EXPECT_EQ("Krasnogorsk", context.GetAddressData().GetCity());
+    EXPECT_EQ("Krasnogorskiy", context.GetAddressData().GetDistrict());
+    EXPECT_EQ("143430", context.GetAddressData().GetIndex());
+    EXPECT_EQ("Russia", context.GetAddressData().GetCountry());
 
     std::string response = parser.ObjectToStr(type_request, context);
 
@@ -94,29 +85,19 @@ TEST(PARSING, PersonalData) {
                          "\"phone_number\":\"+79017453550\", "
                          "\"email\":\"bs@mt11.su\"}}"};
     std::string type_request = {"write_personal"};
-    std::string user_id = {"8"};
-    std::string name = {"Bogdan"};
-    std::string surname = {"Kazimirov"};
-    std::string email = {"bs@mt11.su"};
-    std::string date_birth = {"2000-01-01"};
-    std::string phone_number = {"+79017453550"};
 
     ParserPersonalData parser;
 
     Context context;
 
-    PersonalData personal_data;
-
-    context = personal_data;
-
     context = parser.StrToObject(input);
 
-    EXPECT_EQ(user_id, context.GetPersonalData().GetUserId());
-    EXPECT_EQ(name, context.GetPersonalData().GetName());
-    EXPECT_EQ(surname, context.GetPersonalData().GetSurname());
-    EXPECT_EQ(email, context.GetPersonalData().GetEmail());
-    EXPECT_EQ(date_birth, context.GetPersonalData().GetDateBirth());
-    EXPECT_EQ(phone_number, context.GetPersonalData().GetPhoneNumber());
+    EXPECT_EQ("8", context.GetPersonalData().GetUserId());
+    EXPECT_EQ("Bogdan", context.GetPersonalData().GetName());
+    EXPECT_EQ("Kazimirov", context.GetPersonalData().GetSurname());
+    EXPECT_EQ("bs@mt11.su", context.GetPersonalData().GetEmail());
+    EXPECT_EQ("2000-01-01", context.GetPersonalData().GetDateBirth());
+    EXPECT_EQ("+79017453550", context.GetPersonalData().GetPhoneNumber());
 
     std::string response = parser.ObjectToStr(type_request, context);
 
@@ -140,27 +121,18 @@ TEST(PARSING, GeneralData) {
                          "\"description\":\"MT11-72\", "
                          "\"nickname\":\"asdasd\"}}"};
     std::string type_request = {"write_general"};
-    std::string user_id = {"8"};
-    std::string status = {"asdas"};
-    std::string tags = {"1asd"};
-    std::string description = {"MT11-72"};
-    std::string nickname = {"asdasd"};
 
     ParserGeneralData parser;
 
     Context context;
 
-    GeneralData general_data;
-
-    context = general_data;
-
     context = parser.StrToObject(input);
 
-    EXPECT_EQ(user_id, context.GetGeneralData().GetUserId());
-    EXPECT_EQ(status, context.GetGeneralData().GetStatus());
-    EXPECT_EQ(tags, context.GetGeneralData().GetTags());
-    EXPECT_EQ(description, context.GetGeneralData().GetDescription());
-    EXPECT_EQ(nickname, context.GetGeneralData().GetNickname());
+    EXPECT_EQ("8", context.GetGeneralData().GetUserId());
+    EXPECT_EQ("asdas", context.GetGeneralData().GetStatus());
+    EXPECT_EQ("1asd", context.GetGeneralData().GetTags());
+    EXPECT_EQ("MT11-72", context.GetGeneralData().GetDescription());
+    EXPECT_EQ("asdasd", context.GetGeneralData().GetNickname());
 
     std::string response = parser.ObjectToStr(type_request, context);
 
@@ -178,26 +150,62 @@ TEST(PARSING, GeneralData) {
 }
 
 TEST(PARSING, Events) {
-
+//    std::string input = {"{\"add_event\":[{\"event_name\":\"breakfast\", "
+//                         "\"event_date\":\"01-06-2000\", "
+//                         "\"description\":\"dfhsdftjsftksft\", "
+//                         "\"time_begin\":\"15:45\", "
+//                         "\"time_end\":\"16:45\", "
+//                         "\"user_id\":\"56\"}]}, "
+//                         "{\"event_name\":\"Lancj\", "
+//                         "\"event_date\":\"01-06-2000\", "
+//                         "\"description\":\"2132\", "
+//                         "\"time_begin\":\"11:45\", "
+//                         "\"time_end\":\"14:00\", "
+//                         "\"user_id\":\"56\"}]"};
+//    std::string type_request = {"add_event"};
+//    std::string user_id = {"56"};
+//    std::set<std::string> contacts = {"Ibragim", "Ahper"};
+//
+//    ParserEvent parser;
+//
+//    Context context;
+//
+//    std::set<Event> events = {};
+//
+//    context = events;
+//
+//    context = parser.StrToObject(input);
+//
+//    EXPECT_EQ(user_id, context.GetContacts().GetUserId());
+//    EXPECT_EQ(contacts, context.GetContacts().GetContacts());
+//
+//    std::string response = parser.ObjectToStr(type_request, context);
+//
+//    std::string expected_response = {"{\"add_contact\":\"OK\"}"};
+//
+//    EXPECT_EQ(response, expected_response);
+//
+//    context.SetError("Test error");
+//
+//    std::string expected_response_error = {"{\"add_contact\":\"Test error\"}"};
+//
+//    std::string response_error = parser.ObjectToStr(type_request, context);
+//
+//    EXPECT_EQ(response_error, expected_response_error);
 }
 
 TEST(PARSING, UserContacts) {
     std::string input = {"{\"add_contact\":{\"user_id\":\"56\",\"contacts\":[\"Ibragim\", \"Ahper\"]}}"};
     std::string type_request = {"add_contact"};
-    std::string user_id = {"56"};
     std::set<std::string > contacts = {"Ibragim", "Ahper"};
 
     ParserUserContacts parser;
 
     Context context;
 
-    Contacts cnt;
-
-    context = cnt;
-
     context = parser.StrToObject(input);
 
-    EXPECT_EQ(user_id, context.GetContacts().GetUserId());
+    EXPECT_EQ("56", context.GetContacts().GetUserId());
     EXPECT_EQ(contacts, context.GetContacts().GetContacts());
 
     std::string response = parser.ObjectToStr(type_request, context);
