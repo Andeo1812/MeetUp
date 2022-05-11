f3abaeeeae3952614458f5dfdfadca58814bbfaa
Andeo1812 <alexanforall@gmail.com>
2022-05-11 18:33:05 +0300

    DB

--- project/database/PostgreSQL/src/DBUserImpl.cpp
+++ project/database/PostgreSQL/src/DBUserImpl.cpp
@@ -1,8 +1,28 @@
 #include "DBUserImpl.hpp"
 #include "DBManagerPG.hpp"
 
+#include <iostream>
+
 int DBUserImpl::Registration(const User &user) const {
-    //  auto &con = Singleton<DBManagerPG>::GetInstance().GetData().GetFreeConnection()->GetConnection();
+    std::cout << Singleton<DBManagerPG>::GetInstance().GetData().Size() << std::endl;
+
+    auto &con = Singleton<DBManagerPG>::GetInstance().GetData().GetFreeConnection()->GetConnection();
+
+    std::string sql = "SELECT * FROM user_m";
+
+    std::cout << Singleton<DBManagerPG>::GetInstance().GetData().Size() << std::endl;
+
+    pqxx::work w(con);
+
+    pqxx::result R(w.exec(sql));
+
+    for (auto row : R) {
+        std::cout << "ID = " << row[0].as<std::string>() << std::endl;
+        std::cout << "Nickname = " << row[1].as<std::string>() << std::endl;
+        std::cout << "Password = " << row[2].as<std::string>() << std::endl;
+    }
+
+    std::cout << con.is_open() << std::endl;
 
     return EXIT_SUCCESS;
 }
--- project/main.cpp
+++ project/main.cpp
@@ -5,25 +5,9 @@
 int main(int argc, const char *argv[]) {
     std::cout << "Starting server..." << std::endl;
 
-    std::cout << Singleton<DBManagerPG>::GetInstance().GetData().Size() << std::endl;
+    User user;
 
-    auto &con = Singleton<DBManagerPG>::GetInstance().GetData().GetFreeConnection()->GetConnection();
-
-    std::string sql = "SELECT * FROM user_m";
-
-    std::cout << Singleton<DBManagerPG>::GetInstance().GetData().Size() << std::endl;
-
-    pqxx::work w(con);
-
-    pqxx::result R(w.exec(sql));
-
-    for (auto row : R) {
-        std::cout << "ID = " << row[0].as<std::string>() << std::endl;
-        std::cout << "Nickname = " << row[1].as<std::string>() << std::endl;
-        std::cout << "Password = " << row[2].as<std::string>() << std::endl;
-    }
-
-    std::cout << con.is_open() << std::endl;
+    Singleton<DBManagerPG>::GetInstance().GetData().User.Registration(user);
 
     return EXIT_SUCCESS;
 }
