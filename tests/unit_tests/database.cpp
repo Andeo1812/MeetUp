39f8e96c66b46058d1e4fd5919a64ee77af74e50
Andeo1812 <alexanforall@gmail.com>
2022-05-12 00:23:12 +0300

    DB user YES

--- tests/unit_tests/database.cpp
+++ tests/unit_tests/database.cpp
@@ -1,60 +1,87 @@
 #include <gtest/gtest.h>
 
-//#include "DBUserImpl.hpp"
-//#include "DBUserDataImpl.hpp"
-//#include "DBEventImpl.hpp"
-//#include "DBContactsImpl.hpp"
-//#include "DBGroupImpl.hpp"
-//#include "DBSynchroClientImpl.hpp"
-//#include "PGConnection.hpp"
+#include <unistd.h>
+#include <random>
+
+#include "DBManagerPG.hpp"
+#include "DBUserImpl.hpp"
+#include "DBUserDataImpl.hpp"
+#include "DBEventImpl.hpp"
+#include "DBContactsImpl.hpp"
+#include "DBGroupImpl.hpp"
+#include "DBSynchroClientImpl.hpp"
 
 
-//TEST(PostgreSQL, Connection) {
-//    PGConnection pg_con;
-//
-//    EXPECT_TRUE(pg_con != nullptr);
-//}
-//
-//TEST(PostgreSQL, GetField) {
-//    DBRegAuthImpl reg_auth_db;
-//
-//    EXPECT_TRUE(reg_auth_db);
-//
-//    std::string user_id = {"123"};
-//    std::string password = {"GogozikPARSSS"};
-//    std::string nickname = {"Gogozik"};
-//
-//    User user;
-//
-//    user.SetUserId(user_id);
-//    user.SetPassword(password);
-//    user.SetNickname(nickname);
-//
-//    EXPECT_TRUE(!reg_auth_db.Registration(user));
-//    EXPECT_TRUE(!reg_auth_db.Authentication(user));
-//}
-//
-//
-//TEST(PostgreSQL, DBUserData) {
-//
-//}
-//
-//TEST(PostgreSQL, DBRegAuth) {
-//
-//}
-//
-//TEST(PostgreSQL, DBEvent) {
-//
-//}
-//
-//TEST(PostgreSQL, DBContacts) {
-//
-//}
-//
-//TEST(PostgreSQL, DBGroup) {
-//
-//}
-//
-//TEST(PostgreSQL, DBSynchroClient) {
-//
-//}
+TEST(PostgreSQL, DBManager) {
+    Singleton<DBManagerPG>::GetInstance();
+
+    const size_t numCPU = MAX_COUNT_FREE_DB_VERSION;
+
+    //  const size_t numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().Size(), numCPU);
+
+    auto con = Singleton<DBManagerPG>::GetInstance().GetData().GetFreeConnection();
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().Size(), numCPU - 1);
+
+    Singleton<DBManagerPG>::GetInstance().GetData().InsertConnection(con);
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().Size(), numCPU);
+}
+
+TEST(PostgreSQL, DBUser) {
+    std::string password = {"TEST_DEV"};
+    std::string nickname = {"TEST_DEV"};
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(80,100);
+
+    std::string random_add = std::to_string(dist(gen));
+
+    password += random_add;
+    nickname += random_add;
+
+    User user;
+    user.SetPassword(password);
+    user.SetNickname(nickname);
+
+    std::string new_user_id = Singleton<DBManagerPG>::GetInstance().GetData().User.Registration(user);
+    user.SetId(new_user_id);
+
+    EXPECT_TRUE(!new_user_id.empty());
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().User.Authentication(user), SUCCESS);
+
+    User user_not_exist;
+    user_not_exist.SetPassword({"user_not_exist"});
+    user_not_exist.SetNickname({"user_not_exist"});
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().User.Authentication(user_not_exist), NOT_AUTHENTICATION);
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().User.GetId(user), user.GetId());
+
+    EXPECT_EQ(Singleton<DBManagerPG>::GetInstance().GetData().User.GetNickname(user), user.GetNickname());
+}
+
+
+TEST(PostgreSQL, DBUserData) {
+
+}
+
+TEST(PostgreSQL, DBEvent) {
+
+}
+
+TEST(PostgreSQL, DBContacts) {
+
+}
+
+TEST(PostgreSQL, DBGroup) {
+
+}
+
+TEST(PostgreSQL, DBSynchroClient) {
+
+}
