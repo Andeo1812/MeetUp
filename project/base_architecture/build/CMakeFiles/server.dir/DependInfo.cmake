2a492d06fd3437834ea977266aee84334e2ad4fd
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-05-14 00:53:36 +0300

    change folders structure, add functional to future merge

---
+++ project/base_architecture/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/AsyncServer.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/AsyncServer.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/ClientConnection.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/ClientConnection.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/ConnectionClientManager.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/ConnectionClientManager.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/MimeTypes.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/MimeTypes.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/Reply.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/Reply.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/RequestHandler.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/RequestHandler.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/RequestParser.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/RequestParser.cpp.o"
+  "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/src/main.cpp" "/media/ivan/6A0CEC5E0CEC26B3/uchyoba/TexnoPark/Osnova/C-C++/MeetUp_Official/project/base_architecture/build/CMakeFiles/server.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_ATOMIC_DYN_LINK"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "BOOST_THREAD_DYN_LINK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/virtual"
+  "../include/impl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/base_architecture/include/impl/HttpRequest.hpp
@@ -0,0 +1,41 @@
+
+
+#ifndef HTTP_REQUEST_HPP
+#define HTTP_REQUEST_HPP
+
+#include <string>
+#include <vector>
+#include "Header.hpp"
+#include <iostream>
+
+namespace http {
+namespace AsyncServer {
+
+/// A request received from a client.
+struct HttpRequest {
+    std::string method;
+    std::string uri;
+    int http_version_major;
+    int http_version_minor;
+    std::vector<Header> headers;
+    std::string content;
+
+    void print_req() const {
+        std::cout << "***REQUEST***" << std::endl;
+        std::cout << "method: " << method << std::endl;
+        std::cout << "uri: " << uri << std::endl;
+        std::cout << "http_version_major: " << http_version_major << std::endl;
+        std::cout << "http_version_minor: " << http_version_minor << std::endl;
+
+        std::cout << "--------headers---------" << std::endl;
+        for (auto &i : headers) {
+            std::cout << i.name << ": " << i.value << std::endl;
+        }
+        std::cout << "------end_headers-------" << std::endl;
+    }
+};
+
+} // namespace AsyncServer
+} // namespace http
+
+#endif // HTTP_REQUEST_HPP
